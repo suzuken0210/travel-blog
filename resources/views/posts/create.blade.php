f48b3f29a552b1f0e4fdf5d7cdb32d717b4504da
suzuken0210 <suzukikenta0530@gmail.com>
2023-05-15 20:50:04 +0900

    適切なコミットコメント

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,42 +14,50 @@
                 @csrf
                 <div class="title">
                     <h2>タイトル</h2>
-                    <input type="text" name="post[title]" placeholder="タイトル"/>
+                    <input type="text" name="post[title]" placeholder="GWに東京に行ってきました！"/>
                 </div>
-                
-                <div class="address">
-                    <h2>訪れた場所</h2>
-                    <input type="text" name="address[0]" placeholder="tokyo"/>
-                    <input type="text" name="address[1]" placeholder="tokyo"/>
-                    <input type="text" name="address[2]" placeholder="tokyo"/>
-                </div>
-                
-                <div class="spot">
-                    <h2>訪れた場所の中で特にお気に入りの場所</h2>
-                    <input type="text" name="post[spot]" placeholder="tokyo"/>
-                </div>
-                
-                <div class="body">
-                    <h2>本文</h2>
-                    <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
-                    
-                </div>
-                
-                
                 
                 <div class="schedule">
                     <h2>旅行日</h2>
                     <input type="date" name="post[schedule]"/>
                 </div>
                 
+                <div class="address">
+                    <h2>訪れた場所（最大１６箇所）</h2>
+                    <input type="text" name="address[0]" placeholder="東京タワー"/>
+                    <input type="text" name="address[1]" placeholder="東京スカイツリー"/>
+                    <input type="text" name="address[2]" placeholder="東京駅"/>
+                    <input type="text" name="address[3]" placeholder="上野動物園"/>
+                    <input type="text" name="address[4]" />
+                    <input type="text" name="address[5]" />
+                    <input type="text" name="address[6]" />
+                    <input type="text" name="address[7]" />
+                    <input type="text" name="address[8]" />
+                    <input type="text" name="address[9]" />
+                    <input type="text" name="address[10]" />
+                    <input type="text" name="address[11]" />
+                    <input type="text" name="address[12]" />
+                    <input type="text" name="address[13]" />
+                    <input type="text" name="address[14]" />
+                    <input type="text" name="address[15]" />
+                </div>
+                
+                <div class="spot">
+                    <h2>訪れた場所の中で特にお気に入りの場所</h2>
+                    <input type="text" name="post[spot]" placeholder="東京タワー"/>
+                </div>
+                
                 <div class="image">
+                    <h2>お気に入り観光地の写真</h2>
                 <input type="file" name="image">
                 </div>
                 
-                <div class="image2">
-                <input type="file" name="image2">
+                <div class="body">
+                    <h2>旅行日記</h2>
+                    <textarea name="post[body]" placeholder=""></textarea>
+                    
                 </div>
-                
+ 
                 <input type="submit" value="保存"/>
             </form>
             <div class="footer">
